c0373a659b4696db4cd042f4c4c21c3fa478004d
Atlis-Developer <utts.kyiv@gmail.com>
2021-11-08 18:28:32 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ coordinatesSnake[0] = {
 }
 
 
+
 let coordinatesApple = {
     x: Math.floor(Math.random() * 17 + 1) * sizeBox,
     y: Math.floor(Math.random() * 15 + 3) * sizeBox,
@@ -45,15 +46,72 @@ let coordinatesApple = {
 function paintAll() {
     snk.drawImage(img, 0, 0);
     snk.drawImage(apple, coordinatesApple.x, coordinatesApple.y);
-    snk.drawImage(headSnake, coordinatesSnake[0].x, coordinatesSnake[0].y)
 
+    for (let i = 0; i < coordinatesSnake.length; i++) {
+        if (i == 0) {
+            snk.drawImage(headSnake, coordinatesSnake[i].x, coordinatesSnake[i].y)
+        }else{
+            snk.fillStyle = 'green';
+            snk.fillRect(coordinatesSnake[i].x, coordinatesSnake[i].y, sizeBox, sizeBox);
+        }    
+    }
+    
     snk.fillStyle = 'white';
-    snk.font = '50px Arial'
-    snk.fillText(result, 2 * sizeBox, 1.7 * sizeBox)
+    snk.font = '50px Arial';
+    snk.fillText(result, 2 * sizeBox, 1.7 * sizeBox);
 
+    let headSnakeX = coordinatesSnake[0].x;
+    let headSnakeY = coordinatesSnake[0].y;
+
+    if (coordinatesApple.x == headSnakeX && coordinatesApple.y == headSnakeY) {
+        result++;
+        coordinatesApple = {
+            x: Math.floor(Math.random() * 17 + 1) * sizeBox,
+            y: Math.floor(Math.random() * 15 + 3) * sizeBox,
+        };
+    } else {
+        coordinatesSnake.pop()
+    }
+
+
+
+    switch (route) {
+        case 'left':
+            headSnakeX -= sizeBox;
+            break;
+        case 'right':
+            headSnakeX += sizeBox;
+            break;
+        case 'up':
+            headSnakeY -= sizeBox;
+            break;
+        case 'down':
+            headSnakeY += sizeBox;
+            break;
+    }
+
+    if(headSnakeX < 0 || headSnakeX > (sizeBox * 18) + 1 ||  headSnakeY < sizeBox * 2 ||         headSnakeY > (sizeBox * 18) + 3){
+        clearInterval(game);
+        alert('You lose!')
+    }
+
+    let newHeadSnake = {
+        x: headSnakeX,
+        y: headSnakeY
+    };
+
+    for (let i = 0; i < coordinatesSnake.length; i++) {
+        if(coordinatesSnake[i].x == newHeadSnake.x && coordinatesSnake[i].y == newHeadSnake.y){
+            clearInterval(game);
+            alert('You lose!')
+        }
+        
+    }
+
+    coordinatesSnake.unshift(newHeadSnake)
 }
 
 
 
 
-setInterval(paintAll, 100)
+let game = setInterval(paintAll, 200)
